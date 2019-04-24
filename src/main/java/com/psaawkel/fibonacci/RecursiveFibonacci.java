c8e242f4bf710c257885f789b7e700e0351693ed
Paweł Sak <sak.pawel94@gmail.com>
2019-04-24 20:56:52 +0200

    Started development after setup

--- src/main/java/com/psaawkel/fibonacci/RecursiveFibonacci.java
+++ src/main/java/com/psaawkel/fibonacci/RecursiveFibonacci.java
@@ -3,6 +3,10 @@ package com.psaawkel.fibonacci;
 public class RecursiveFibonacci implements IFibonacci {
 
     public int calculate(int factor) {
+
+        if(factor==2) return 1+0;
+
+        if(factor==1) return 1;
         return 0;
     }
 }
--- src/test/java/com/psaawkel/fibonacci/RecursiveFibonacciTest.java
+++ src/test/java/com/psaawkel/fibonacci/RecursiveFibonacciTest.java
@@ -14,6 +14,16 @@ public class RecursiveFibonacciTest {
         assertEquals(0,fibo.calculate(0));
     }
 
+    @Test
+    void oneTest(){
+        assertEquals(1,fibo.calculate(1));
+    }
+
+    @Test
+    void twoTest(){
+        assertEquals(1,fibo.calculate(2));
+    }
+
     @BeforeAll
     public static void prepareForTesting(){
         fibo = new RecursiveFibonacci();
